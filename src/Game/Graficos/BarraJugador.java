7827428c5b43806c1ff01163c9f5b4ed9ef99ce2
Diego Vidal <vidalaguilardiego@ciencias.unam.mx>
2023-11-07 11:21:15 -0600

    Barra de vida

--- src/Game/Graficos/BarraJugador.java
+++ src/Game/Graficos/BarraJugador.java
@@ -32,12 +32,40 @@ public class BarraJugador {
 
   public void dibujar(final Graphics g){
     dibujarBarraVitalidad(g);
+    dibujarPuntos(g);
+    dibujarRanuraObjetos(g);
   }
 
   private void dibujarBarraVitalidad(final Graphics g){
     final int medidaVertical = 4; 
     final int anchoTotal = 100;
 
-    DibujoDebug.dibujarRectanguloRelleno(g. areaInventario.x + 35, areaInventario.y + medidaVertical, anchoTotal, rojoOscuro);
+    DibujoDebug.dibujarRectanguloRelleno(g, areaInventario.x + 35, areaInventario.y + medidaVertical, anchoTotal, rojoClaro);
+    DibujoDebug.dibujarRectanguloRelleno(g, areaInventario.x + 35, areaInventario.y + medidaVertical * 2, anchoTotal, medidaVertical, rojoOscuro);
+    
+    g.setColor(Color.white);
+    
+    DibujoDebug.dibujarString(g, "Health", areaInventario.x + 10, areaInventario.y + medidaVertical*3,);
+    DibujoDebug.dibujarString(g, "1000",anchoTotal + 45, areaInventario.y + medidaVertical*3);
+  }
+  private void dibujarPuntos(final Graphics g){
+    final int medidaVertical = 4; 
+    //DibujoDebug.dibujarString(g, "Puntos: " + AquiDebemosReferenciarAljugador, areaInventario.x + 10,areaInventario.y +medidaVertical * 15, Color.white);
+  }
+  // Sera ocupadoncomo ranura del ayudante 
+  private void dibujarRanuraObjetos(final Graphics g){
+    final int anchoRanura = 32; 
+    final int numeroRanuras = 1;
+    final int espaciadoRanuras = 10; 
+    final int anchoTotal = anchoRanura*numeroRanuras + espaciadoRanuras*numeroRanuras; 
+    final int xInicial = Constantes.ANCHO_PANTALLA - anchoTotal;
+    final int anchoRanuraYEspacio = anchoRanura + espaciadoRanuras; 
+    g.setColor(Color.white);
+    for(int i = 0; i < numeroRanuras; i++){
+      int xActual = xInicial + anchoRanuraYEspacio*i; 
+      Rectangle ranura = new Rectangle(xActual, areaInventario.y + 4, anchoRanura, anchoRanura);
+      DibujoDebug.dibujarRectanguloRelleno(g,ranura);
+      DibujoDebug.dibujarString(g, "" + i, xActual + 13, areaInventario.y + 54);
+    }
   }
 }
