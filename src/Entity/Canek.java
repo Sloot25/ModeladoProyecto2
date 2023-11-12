ebb7a2e363ef97b22ea5f375aaa2a75e608a4327
Diego Vidal <vidalaguilardiego@ciencias.unam.mx>
2023-11-12 16:31:54 -0600

    Agregue a canek

--- src/Entity/Canek.java
+++ src/Entity/Canek.java
@@ -4,11 +4,11 @@ import Game.GamePanel;
 
 public class Canek extends NPC{
   public Canek(GamePanel gp, int x, int y, int width, int height){
-    super(gp, x, y, width, height);
+    super(gp, x, y, width*2, height*2);
   }
   public void getEntityImage(){
     // Ingresar la ruta del personaje de canek
-    imagen = gp.getRutas().getImagen("canek.png");
+    imagen = gp.getRutas().getImagen("Canek.png");
   }
   public void interact(){
     gp.player.startTalking("¿Ya acabaste con las chinches? ¿No? Estoy muy decepcionado de ti");
